f6290475bf27e1945bdb0918d214b0b1e7c51a34
Aleksandr Sakharov <real-rp.ru@mail.ru>
2019-05-06 01:33:18 +0300

    up

--- instructions.js
+++ instructions.js
@@ -3,7 +3,7 @@
 const { join } = require('path')
 
 module.exports = async function (cli) {
-  await cli.makeConfig('telegraf.js', join(__dirname, './config/telegram.js'))
+  await cli.makeConfig('telegram.js', join(__dirname, './config/telegram.js'))
     .catch((e) => {})
   cli.command.completed('create', 'config/telegram.js')
 }
